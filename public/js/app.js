f5da27502d40aaf6c3b7b0f9adfe26e5f14d79ee
Paulius Lazdauskas <paulius.lazdauskas1@gmail.com>
2021-12-07 13:26:26 +0200

    second commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,3 @@
-console.log("Client side JS file loaded");
-
-// fetch("http://localhost:3000/weather?address=!").then((res) =>
-//   res.json().then((data) => {
-//     if (data.error) {
-//       console.log(data.error);
-//     } else {
-//       console.log(data.location);
-//       console.log(data.forecast);
-//     }
-//   })
-// );
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.getElementById("message-1");
